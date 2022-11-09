9ab4f6697708665642a42189500e14ea29b602a7
diegorafaelgit <diegorafael19977@gmail.com>
2022-11-08 22:18:05 -0300

    logo e responsividade feitas

--- js/index.js
+++ js/index.js
@@ -1,20 +1,16 @@
-$('#fecha_modal').click(function () {
-    $('#modal_habilidades').hide()
-})
-
 $('#img_promogames').click(function () {
-    $('#modal_projetos').slideToggle(500)
+    $('#modal_projetos').modal('show')
     $('#modal_titulo2').text('PROMOGAMES')
     $('#modal_conteudo2').text('Promogames é um protótipo de alta fidelidade de um aplicativo mobile sobre jogos\
     e seu principal objetivo é ser um app de notificação de promoções de jogos da Ps Store, Xbox Store, Steam, entre outras.')
-    $('#link').prop('href', 'https://www.figma.com/file/osmVC6TgiWfj0L7Dizgfgk/Trabalho-Leonardo?node-id=0%3A1')
     $('#img_carrossel1').prop('src', 'images/promogames1.jpg')
     $('#img_carrossel2').prop('src', 'images/promogames2.jpg')
     $('#img_carrossel3').prop('src', 'images/promogames3.jpg')
-
+    $('#link').prop('href', 'https://www.figma.com/file/osmVC6TgiWfj0L7Dizgfgk/Trabalho-Leonardo?node-id=0%3A1')
 })
+
 $('#img_agatetepe').click(function () {
-    $('#modal_projetos').slideToggle(500)
+    $('#modal_projetos').modal('show')
     $('#modal_titulo2').text('AGATETEPÊ')
     $('#modal_conteudo2').text('Agatetepê é um website sobre tecnologias, em foco sobre assistentes virtuais, trazendo em destaque\
     o Echo Dot da Amazon e suas funcionalidades.')
@@ -24,7 +20,7 @@ $('#img_agatetepe').click(function () {
     $('#img_carrossel3').prop('src', 'images/agatetepe3.jpg')
 })
 $('#img_meuplay').click(function () {
-    $('#modal_projetos').slideToggle(500)
+    $('#modal_projetos').modal('show')
     $('#modal_titulo2').text('MEUPLAY')
     $('#modal_conteudo2').text('Meuplay é um website de noticias sobre jogos com foco no Playstation. Seus artigos contém\
     ferramentas de acessibilidade para pessoas com deficiências visual e auditiva.')
@@ -32,13 +28,6 @@ $('#img_meuplay').click(function () {
     $('#img_carrossel1').prop('src', 'images/meuplay1.jpg')
     $('#img_carrossel2').prop('src', 'images/meuplay2.jpg')
     $('#img_carrossel3').prop('src', 'images/meuplay3.jpg')
-})
-
-$('#fecha_modal2').click(function () {
-    $('#modal_projetos').hide()
-})
-$('#btn_responsivo').click(function () {
-    $('.show').remove()
 })
 
 function typeWrite(elemento) {
