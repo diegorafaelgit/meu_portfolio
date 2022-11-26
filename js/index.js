22139ffb9a5e87e53fc50af976a760fab6b2b0c2
diegorafaelgit <diegorafael19977@gmail.com>
2022-11-25 22:13:59 -0300

    Mudanças pós apresentação Fabio

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+$('form').submit(function () {
+    swal({
+        title: "Mensagem enviada com sucesso!!!",
+    });
+})
+
 $('#btn_promogames').click(function () {
     $('#modal_projetos').modal('show')
-    $('#modal_titulo2').text('PROMOGAMES')
+    $('#modal_titulo2').text('Promogames')
+    $('#criado_em').text('Criado em: 1° Semestre de 2021;')
+    $('#criado_por').text('Criado por: Diego Thomas; Wagner Pitelli; Fábio Bortolança;')
+    $('#ferramentas').text('Tecnologia Utilizada: Figma;')
     $('#modal_conteudo2').text('Promogames é um protótipo de alta fidelidade de um aplicativo mobile sobre jogos\
     e seu principal objetivo é ser um app de notificação de promoções de jogos da Ps Store, Xbox Store, Steam, entre outras.')
     $('#img_carrossel1').prop('src', 'images/promogames1.jpg')
@@ -11,7 +20,10 @@ $('#btn_promogames').click(function () {
 
 $('#btn_agatetepe').click(function () {
     $('#modal_projetos').modal('show')
-    $('#modal_titulo2').text('AGATETEPÊ')
+    $('#modal_titulo2').text('Agatetepê')
+    $('#criado_em').text('Criado em: 2° Semestre de 2022;')
+    $('#criado_por').text('Criado por: Diego Thomas; Fábio Bortolança; Nicolas Chinaglia;')
+    $('#ferramentas').text('Tecnologias Utilizadas: HTML, CSS, SASS, JavaScript;')
     $('#modal_conteudo2').text('Agatetepê é um website sobre tecnologias, em foco sobre assistentes virtuais, trazendo em destaque\
     o Echo Dot da Amazon e suas funcionalidades.')
     $('#link').prop('href', 'https://github.com/diegorafaelgit/Agatetep-')
@@ -22,7 +34,10 @@ $('#btn_agatetepe').click(function () {
 
 $('#btn_meuplay').click(function () {
     $('#modal_projetos').modal('show')
-    $('#modal_titulo2').text('MEUPLAY')
+    $('#modal_titulo2').text('MeuPlay')
+    $('#criado_em').text('Criado em: 1° Semestre de 2021;')
+    $('#criado_por').text('Criado por: Diego Thomas; Fábio Bortolança;')
+    $('#ferramentas').text('Tecnologias Utilizadas: HTML, CSS, JavaScript, vLibras;')
     $('#modal_conteudo2').text('Meuplay é um website de noticias sobre jogos com foco no Playstation. Seus artigos contém\
     ferramentas de acessibilidade para pessoas com deficiências visual e auditiva.')
     $('#link').prop('href', 'https://github.com/diegorafaelgit/MeuPlay')
@@ -95,7 +110,24 @@ $('#card_git').on('mouseout', function () {
     $('#hab_itens_titulo').text('* Passe o mouse em cima do logo para saber mais sobre a ferramenta *');
     $('#hab_texto').text('');
 });
+$('#card_figma').on('mouseover', function () {
+    $('#hab_itens_titulo').text('Figma');
+    $('#hab_texto').text('Figma é um editor gráfico de vetor e prototipagem de projetos de design baseado principalmente no navegador web, com ferramentas offline adicionais para aplicações desktop para GNU/Linux, macOS e Windows.');
+});
+$('#card_figma').on('mouseout', function () {
+    $('#hab_itens_titulo').text('* Passe o mouse em cima do logo para saber mais sobre a ferramenta *');
+    $('#hab_texto').text('');
+});
+$('#card_jquery').on('mouseover', function () {
+    $('#hab_itens_titulo').text('jQuery');
+    $('#hab_texto').text('jQuery é uma biblioteca livre que contém funções da linguagem de programação JavaScript que interage com páginas em HTML, desenvolvida para simplificar os scripts executados/interpretados no navegador de internet do usuário.');
+});
+$('#card_jquery').on('mouseout', function () {
+    $('#hab_itens_titulo').text('* Passe o mouse em cima do logo para saber mais sobre a ferramenta *');
+    $('#hab_texto').text('');
+});
 
+// Efeito de máquina de escrever no nome
 function typeWrite(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = ' ';
@@ -125,6 +157,7 @@ const debounce = function (func, wait, immediate) {
     };
 };
 
+// Efeito de entrada de tela animação 
 const target = document.querySelectorAll('[data-anime]')
 const animationClass = 'animate'
 
